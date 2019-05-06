9a30b32111f69acbb2e53c8e12c7fc4489784e7d
luojinxu <382967255@qq.com>
2019-05-06 21:55:15 +0800

    lessons1

---
+++ lessons1/main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron')
+
+let onewin = null
+let twowin = null
+
+app.on('ready', () => {
+    
+    var WindowOption = {
+        width:400,
+        height:400,
+        webPreferences:{
+            nodeIntegration:true
+       }
+    }
+    onewin = new BrowserWindow(WindowOption)
+    onewin.webContents.openDevTools()
+    onewin.loadFile('index.html')
+
+    onewin.on('closed',() => {
+        win = null
+    })
+
+    twowin = new BrowserWindow(WindowOption)
+    twowin.loadFile('new.html')
+
+    twowin.on('closed',() => {
+        win = null
+    })
+})
+
+
+
