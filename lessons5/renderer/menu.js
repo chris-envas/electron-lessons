8324ae21365a4ea8a49a885d8ea643b7d12e2c76
luojinxu <382967255@qq.com>
2019-05-31 20:03:12 +0800

    fix

--- lessons5/renderer/menu.js
+++ lessons5/renderer/menu.js
@@ -1,165 +1,54 @@
-const {Menu} = require('electron').remote;
-var template = [
+//在渲染进程调用Menu模块
+const {Menu}=require('electron').remote;
+
+//定义菜单
+var template=[
     {
-      label: 'Edit',
-      submenu: [
-        {
-          label: 'Undo',
-          accelerator: 'CmdOrCtrl+Z',
-          role: 'undo'
-        },
-        {
-          label: 'Redo',
-          accelerator: 'Shift+CmdOrCtrl+Z',
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Cut',
-          accelerator: 'CmdOrCtrl+X',
-          role: 'cut'
-        },
-        {
-          label: 'Copy',
-          accelerator: 'CmdOrCtrl+C',
-          role: 'copy'
-        },
-        {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
-          role: 'paste'
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          role: 'selectall'
-        },
-      ]
+
+        label:'文件',
+        submenu:[
+            {
+                label:'新建文件',
+                // 注册快捷键
+                accelerator:'ctrl+n',
+                // 点击触发回调
+                click:function(){ 
+                    console.log('ctrl+n');
+                }
+            },
+            {
+                label:'新建窗口',
+                click:function(){ 
+                    console.log('new window');
+                }
+            }
+        ]
     },
     {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click: function(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.reload();
-          }
-        },
-        {
-          label: 'Toggle Full Screen',
-          accelerator: (function() {
-            if (process.platform == 'darwin')
-              return 'Ctrl+Command+F';
-            else
-              return 'F11';
-          })(),
-          click: function(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-          }
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator: (function() {
-            if (process.platform == 'darwin')
-              return 'Alt+Command+I';
-            else
-              return 'Ctrl+Shift+I';
-          })(),
-          click: function(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.toggleDevTools();
-          }
-        },
-      ]
-    },
-    {
-      label: 'Window',
-      role: 'window',
-      submenu: [
-        {
-          label: 'Minimize',
-          accelerator: 'CmdOrCtrl+M',
-          role: 'minimize'
-        },
-        {
-          label: 'Close',
-          accelerator: 'CmdOrCtrl+W',
-          role: 'close'
-        },
-      ]
-    },
-    {
-      label: 'Help',
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: function() { require('electron').shell.openExternal('http://electron.atom.io') }
-        },
-      ]
-    },
-  ];
-  
-  if (process.platform == 'darwin') {
-    var name = require('electron').remote.app.getName();
-    template.unshift({
-      label: name,
-      submenu: [
-        {
-          label: 'About ' + name,
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Services',
-          role: 'services',
-          submenu: []
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Hide ' + name,
-          accelerator: 'Command+H',
-          role: 'hide'
-        },
-        {
-          label: 'Hide Others',
-          accelerator: 'Command+Alt+H',
-          role: 'hideothers'
-        },
-        {
-          label: 'Show All',
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click: function() { app.quit(); }
-        },
-      ]
-    });
-    // Window menu.
-    template[3].submenu.push(
-      {
-        type: 'separator'
-      },
-      {
-        label: 'Bring All to Front',
-        role: 'front'
-      }
-    );
-  }
-  
+        label:'编辑',
+        submenu:[
+            {
+                label:'复制11',
+                //使用内置角色
+                role:'copy'
+            },
+            {
+                label:'截切',
+                role:'cut'
+            }
+        ]
+    }
+]
+
+
 var menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
+
+
+//右键菜单事件
+window.addEventListener('contextmenu',function(e){
+    //阻止当前窗口默认事件
+    e.preventDefault();
+    //在当前窗口点击右键的时候弹出  定义的菜单模板
+    menu.popup({window:remote.getCurrentWindow()})
+},false)
